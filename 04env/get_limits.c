963a9f5ec5300be1964fc0a2acf20c6fb0ba5093
Leteno <changchunjunhao@163.com>
2015-05-24 20:56:50 +0800

    Sun May 24 20:56:50 CST 2015 04env

---
+++ 04env/get_limits.c
@@ -0,0 +1,12 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("NAME_MAX is %d\n", NAME_MAX);
+	printf("CHAR_BIT is %d\n", CHAR_BIT);
+	printf("CHAR_MAX is %d\n", CHAR_MAX);
+	printf("INT_MAX is %d\n", INT_MAX);
+
+	return 0;
+}
